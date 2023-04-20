a12c57afbac55252e546f8f7dacfb93a992ae77a
Ralf Mackenbach <ralf.mackenbach@gmail.com>
2023-04-20 13:55:15 +0200

    Last updates
    Everything in working order now

--- optimisation/run_optimisation.py
+++ optimisation/run_optimisation.py
@@ -28,6 +28,7 @@ mpi.write()
 # We will start with a circular tokamak without current.
 vmec = Vmec(filename="input.tok",mpi=mpi,verbose=False)
 surf = vmec.boundary
+rand_init = False
 
 # make list in which we will store the targets
 make_tuples = []
@@ -40,9 +41,6 @@ make_tuples.append((make_optimizable(aspect_target, vmec,8.0,False).J, 0.0, 1.0)
 
 # iota target
 make_tuples.append((make_optimizable(iota0_target, vmec, 1.0).J, 0.0, 1.0))
-
-# # qs target
-# make_tuples.append((make_optimizable(qs, vmec, 0.0).J, 0.0, 1.0))
 
 # mirror target
 make_tuples.append((make_optimizable(mirror_ratio_target, vmec, 0.21).J, 0.0, 1.0))
@@ -63,7 +61,7 @@ range_val = 2
 for step in range(range_val):
     max_mode = step + 1
 
-    # VMEC's mpol & ntor will be 3, 3... 2+range_val:
+    # VMEC's mpol & ntor will be 2, 3... 2+range_val:
     vmec.indata.mpol = 2 + step
     vmec.indata.ntor = vmec.indata.mpol
 
@@ -71,6 +69,20 @@ for step in range(range_val):
         print("Beginning optimization with max_mode =", max_mode, \
               ", vmec mpol=ntor=", vmec.indata.mpol, \
               ". Previous vmec iteration = ", vmec.iter)
+
+    if (step == 0) and (rand_init == True):
+        # Randomly perturb the initial condition, only low harmonics
+        max_m = 1
+        max_n = 1
+        surf.fix_all()
+        surf.fixed_range(mmin=0, mmax=max_m,
+                         nmin=-max_n, nmax=max_n, fixed=False)
+        surf.fix("rc(0,0)") # Major radius
+        surf.fix("zs(0,1)")  # Do not allow rotation about the x-axis
+        new_x = surf.x
+        new_x += (np.random.rand(len(new_x)) - 0.5) * 2 * 0.001
+        new_x = mpi.comm_world.bcast(new_x)
+        surf.x = new_x
 
     # Define parameter space:
     surf.fix_all()
@@ -81,7 +93,7 @@ for step in range(range_val):
     # For the test to run quickly, we stop after the first function
     # evaluation, by passing max_nfev=1 to scipy.optimize. For a
     # "real" optimization, remove the max_nfev parameter below.
-    least_squares_mpi_solve(prob, mpi, grad=True)
+    least_squares_mpi_solve(prob, mpi, grad=True,diff_method="centered")
 
     # Preserve the output file from the last iteration, so it is not
     # deleted when vmec runs again:
